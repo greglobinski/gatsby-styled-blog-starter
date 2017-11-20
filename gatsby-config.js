e611e687722708c82c9114c2c5460b70dddb76f1
Greg Lobinski <greglobinski@gmail.com>
2017-11-20 09:06:50 +0100

    posts moved outside src directory now there are in data dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/posts`,
+        path: `${__dirname}/data`,
         name: "posts"
       }
     },
